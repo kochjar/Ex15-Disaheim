5be4aa567b3898a206251fa39fc16f996332ea88
kochjar <timgfx@outlook.com>
2023-03-09 22:13:37 +0100

    Færdig kode

---
+++ Ex15-Disaheim/Controller.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex15_Disaheim
+{
+    public class Controller
+    {
+        public List<Book> Books { get; set; }
+        public List<Amulet> Amulets { get; set; }
+
+        public Controller() {
+            Books = new List<Book>();
+            Amulets = new List<Amulet>();
+        }
+
+        public void AddToList(Book book) {
+            Books.Add(book);
+        }
+
+        public void AddToList(Amulet amulet) { 
+            Amulets.Add(amulet);
+        }
+    }
+}
--- Ex15-Disaheim/Program.cs
+++ Ex15-Disaheim/Program.cs
@@ -7,8 +7,26 @@
             Console.WriteLine("Hello, World!");
             Level level = Level.low;
             Console.Write(level);
+
+            Book b1 = new Book("1");
+            Book b2 = new Book("2", "Falling in Love with Yourself");
+            Book b3 = new Book("3", "Spirits in the Night", 123.55);
+
+            Amulet a1 = new Amulet("11");
+            Amulet a2 = new Amulet("12", Level.high);
+            Amulet a3 = new Amulet("13", Level.low, "Capricorn");
+
+            Controller controller = new Controller();
+
             
-           
+            
+            controller.AddToList(b3);/*
+            controller.AddToList(b2);
+            controller.AddToList(b3);
+
+            controller.AddToList(a1);
+            controller.AddToList(a2);
+            controller.AddToList(a3);*/
         }
     }
 }

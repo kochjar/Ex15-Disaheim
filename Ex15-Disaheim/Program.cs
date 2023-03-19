8109afb4d848ec1a97e6d6f22fc55bef31c72207
kochjar <timgfx@outlook.com>
2023-03-19 21:24:41 +0100

    ØVelse 18

--- Ex15-Disaheim/Program.cs
+++ Ex15-Disaheim/Program.cs
@@ -7,7 +7,34 @@ namespace Ex15_Disaheim
     {
         static void Main(string[] args)
         {
-            
+            Book b1, b2, b3;
+            Amulet a1, a2, a3;
+            Course c1, c2;
+            CourseRepository courses;
+            MerchandiseRepository merchandises;
+
+            b1 = new Book("1");
+            b2 = new Book("2", "Falling in Love with Yourself");
+            b3 = new Book("3", "Spirits in the Night", 123.55);
+            a1 = new Amulet("11");
+            a2 = new Amulet("12", Level.high);
+            a3 = new Amulet("13", Level.low, "Capricorn");
+            c1 = new Course("Eufori med røg");
+            c2 = new Course("Nuru Massage using Chia Oil", 157);
+            courses = new CourseRepository();
+            merchandises = new MerchandiseRepository();
+            merchandises.AddMerchandise(b1);
+            merchandises.AddMerchandise(b2);
+            merchandises.AddMerchandise(b3);
+            Console.WriteLine(merchandises.GetTotalValue());
+            merchandises.AddMerchandise(a1);
+            merchandises.AddMerchandise(a2);
+            merchandises.AddMerchandise(a3);
+            courses.AddCourse(c1);
+            courses.AddCourse(c2);
+
+            Console.WriteLine(merchandises.GetTotalValue());
+            Console.ReadLine();
         }
     }
 }
--- UnitTest_Ex15-Disaheim/UnitTest4.cs
+++ UnitTest_Ex15-Disaheim/UnitTest4.cs
@@ -1,68 +1,84 @@
 ﻿using Ex15_Disaheim;
 
-public class UnitTest4
+namespace UnitTest_Ex15_Disaheim
 {
-    Book b1, b2, b3;
-    Amulet a1, a2, a3;
-    Course c1, c2;
-    CourseRepository courses;
-    MerchandiseRepository merchandises;
-    [TestInitialize]
-    public void Init()
+    [TestClass]
+    public class UnitTest4
     {
-        // Arrange            b1 = new Book("1");
-        b2 = new Book("2", "Falling in Love with Yourself");
-        b3 = new Book("3", "Spirits in the Night", 123.55);
-        a1 = new Amulet("11");
-        a2 = new Amulet("12", Level.high);
-        a3 = new Amulet("13", Level.low, "Capricorn");
-        c1 = new Course("Eufori med røg");
-        c2 = new Course("Nuru Massage using Chia Oil", 157);
-        courses = new CourseRepository();
-        merchandises = new MerchandiseRepository();
-        // Act            merchandises.AddMerchandise(b1);
-        merchandises.AddMerchandise(b2);
-        merchandises.AddMerchandise(b3);
-        merchandises.AddMerchandise(a1);
-        merchandises.AddMerchandise(a2);
-        merchandises.AddMerchandise(a3);
-        courses.AddCourse(c1);
-        courses.AddCourse(c2);
+        Book b1, b2, b3;
+        Amulet a1, a2, a3;
+        Course c1, c2;
+
+        CourseRepository courses;
+        MerchandiseRepository merchandises;
+
+        [TestInitialize]
+        public void Init()
+        {
+            // Arrange
+            b1 = new Book("1");
+            b2 = new Book("2", "Falling in Love with Yourself");
+            b3 = new Book("3", "Spirits in the Night", 123.55);
+
+            a1 = new Amulet("11");
+            a2 = new Amulet("12", Level.high);
+            a3 = new Amulet("13", Level.low, "Capricorn");
+
+            c1 = new Course("Eufori med røg");
+            c2 = new Course("Nuru Massage using Chia Oil", 157);
+
+            courses = new CourseRepository();
+            merchandises = new MerchandiseRepository();
+
+            // Act
+            merchandises.AddMerchandise(b1);
+            merchandises.AddMerchandise(b2);
+            merchandises.AddMerchandise(b3);
+
+            merchandises.AddMerchandise(a1);
+            merchandises.AddMerchandise(a2);
+            merchandises.AddMerchandise(a3);
+
+            courses.AddCourse(c1);
+            courses.AddCourse(c2);
+        }
+
+        [TestMethod]
+        public void TestGetBook()
+        {
+            // Assert
+            Assert.AreEqual(b2, merchandises.GetMerchandise("2"));
+        }
+        [TestMethod]
+        public void TestGetAmulet()
+        {
+            // Assert
+            Assert.AreEqual(a3, merchandises.GetMerchandise("13"));
+        }
+        [TestMethod]
+        public void TestGetCourse()
+        {
+            // Assert
+            Assert.AreEqual(c1, courses.GetCourse("Eufori med røg"));
+        }
+        [TestMethod]
+        public void TestGetTotalValueForBook()
+        {
+            // Assert
+            Assert.AreEqual(123.55, merchandises.GetTotalValue());
+        }
+        [TestMethod]
+        public void TestGetTotalValueForAmulet()
+        {
+            // Assert
+            Assert.AreEqual(60.0, merchandises.GetTotalValue());
+        }
+        [TestMethod]
+        public void TestGetTotalValueForCourse()
+        {
+            // Assert
+            Assert.AreEqual(2625.0, courses.GetTotalValue());
+        }
     }
-    [TestMethod]
-    public void TestGetBook()
-    {
-        // Assert
-        Assert.AreEqual(b2, merchandises.GetMerchandise("2"));
-    }
-    [TestMethod]
-    public void TestGetAmulet()
-    {
-        // Assert
-        Assert.AreEqual(a3, merchandises.GetMerchandise("13"));
-    }
-    [TestMethod]
-    public void TestGetCourse()
-    {
-        // Assert
-        Assert.AreEqual(c1, courses.GetCourse("Eufori med røg"));
-    }
-    [TestMethod]
-    public void TestGetTotalValueForBook()
-    {
-        // Assert
-        Assert.AreEqual(123.55, merchandises.GetTotalValue());
-    }
-    [TestMethod]
-    public void TestGetTotalValueForAmulet()
-    {
-        // Assert
-        Assert.AreEqual(60.0, merchandises.GetTotalValue());
-    }
-    [TestMethod]
-    public void TestGetTotalValueForCourse()
-    {
-        // Assert
-        Assert.AreEqual(2625.0, courses.GetTotalValue());
-    }
+
 }
